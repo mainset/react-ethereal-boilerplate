d350e136e67c8bceac22c6a16d68078d2922a5cc
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2019-01-22 15:54:52 +0700

    [fix] SSR: Avoid of copy assets at server

--- config/webpack/config-fragments/ssr-server.config.js
+++ config/webpack/config-fragments/ssr-server.config.js
@@ -7,14 +7,34 @@ var commonConfig = require('./common.config.js');
 var paths = require('./paths');
 /* eslint-enable */
 
-module.exports = merge(commonConfig, {
-  entry: [
-    '@babel/polyfill',
-    path.join(paths.root, 'config', 'server.js'),
-  ],
-  target: 'node',
-  output: {
-    path: path.join(paths.root, 'private'),
-    filename: 'server.js',
+module.exports = merge(
+  commonConfig,
+  {
+    entry: [
+      '@babel/polyfill',
+      path.join(paths.root, 'config', 'server.js'),
+    ],
+    target: 'node',
+    output: {
+      path: path.join(paths.root, 'private'),
+      filename: 'server.js',
+    },
   },
-});
+  {
+    module: {
+      rules: [
+        {
+          test: /\.(svg|png)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                emitFile: false,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  },
+);
