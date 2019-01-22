b7ca9823a353fe3abdc69c66a8530177dc04013d
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2019-01-22 15:54:52 +0700

    [fix] SSR: Avoid of copy assets at server

--- config/webpack/config-fragments/ssr-server.config.mjs
+++ config/webpack/config-fragments/ssr-server.config.mjs
@@ -5,13 +5,33 @@ import paths from '../../paths.mjs';
 
 import commonConfig from './common.config.mjs';
 
-const ssrServerConfig = merge(commonConfig, {
-  entry: ['@babel/polyfill', path.join(paths.rootPath, 'config', 'server.mjs')],
-  target: 'node',
-  output: {
-    path: path.join(paths.rootPath, 'private'),
-    filename: 'server.js',
+const ssrServerConfig = merge(
+  commonConfig,
+  {
+    entry: ['@babel/polyfill', path.join(paths.root, 'config', 'server.mjs')],
+    target: 'node',
+    output: {
+      path: path.join(paths.root, 'private'),
+      filename: 'server.js',
+    },
   },
-});
+  {
+    module: {
+      rules: [
+        {
+          test: /\.(svg|png)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                emitFile: false,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  }
+);
 
 export default ssrServerConfig;
