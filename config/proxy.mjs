58d381665c10c4b51215f04954bb71bd9e1a09ad
Yevhen Uzhva <yevhen.uzhva@gmail.com>
2024-10-26 05:07:54 +0300

    feat: migrate proxy config for webpack and express ssr server to latest v

--- config/proxy.mjs
+++ config/proxy.mjs
@@ -1,10 +1,12 @@
 const proxyConfig = {
   '/api/': {
+    context: ['/api/'],
     target: process.env.API_REMOTE_URL,
     pathRewrite: { '^/api/': '/' },
     changeOrigin: true,
   },
   '/api-local/': {
+    context: ['/api-local/'],
     target: process.env.API_LOCAL_PATH,
     pathRewrite: { '^/api-local/': '/' },
   },
